32791ce06aa584df690a2f69ae247c466cbc82dc
gesha1996 <gesha199696@gmail.com>
2020-03-24 18:06:31 -0500

    Second push after changes

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,5 +2,6 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("First try to push the code");
+        System.out.println("Second push of the code after the changes");
     }
 }
