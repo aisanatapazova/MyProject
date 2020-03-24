4ffa3ecc7114bf6fdffaad1cbd6f9b17f440d746
aisanatapazova <aisa.apazova@gmail.com>
2020-03-24 18:16:53 -0500

    This is third time

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -3,5 +3,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First try to push the code");
         System.out.println("Second push of the code after the changes");
+        System.out.println("Third time trying to push");
     }
 }
